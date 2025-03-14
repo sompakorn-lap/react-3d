fb54af2d100fd030c390320a5986fac82961eb33
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-03-14 11:21:41 +0700

    setup

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,7 +9,7 @@ function Layout() {
   return (
     <div className="grid grid-rows-[auto_1fr_auto] min-h-screen">
       <header className="w-full p-4 text-xl text-center bg-black text-gray-100">
-        <h1>BomSLap - React</h1>
+        <h1>BomSLap - React 3D</h1>
       </header>
 
       <main className="p-2">
@@ -19,7 +19,7 @@ function Layout() {
       <footer className="w-full p-2 bg-black text-gray-100">
         <a
           className="flex gap-2"
-          href="https://github.com/sompakorn-lap/basic-setup-react"
+          href="https://github.com/sompakorn-lap/react-3d"
         >
           <Icon icon="mdi:github" width={24} height={24} />
           <span>GITHUB</span>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,46 @@
 import { createFileRoute } from "@tanstack/react-router";
+import * as THREE from "three";
+import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
+import { useRef, useState } from "react";
 
 export const Route = createFileRoute("/")({
   component: Page,
 });
+
+function Box(props: ThreeElements["mesh"]) {
+  const meshRef = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "#2f74c0"} />
+    </mesh>
+  );
+}
+
 function Page() {
   return (
-    <section>
-      <h1 className="text-xl text-teal-400">Home</h1>
-    </section>
+    <Canvas>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[0, 0, 0]} />
+    </Canvas>
   );
 }
 
